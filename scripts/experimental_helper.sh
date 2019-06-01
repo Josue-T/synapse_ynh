adb96d2e4ab78612697ad1d452321cb85dc853b5
Josué Tille <josue@tille.ch>
2019-06-02 00:09:14 +0200

    Add don't prind password in debug logs

--- scripts/experimental_helper.sh
+++ scripts/experimental_helper.sh
@@ -138,3 +138,4 @@ __PRE_TAG1__$(yunohost tools diagnosis | grep -B 100 "services:" | sed '/service
 	# Send the email to the recipients
 	cat mail_to_send | $mail_bin -a "Content-Type: $content_type; charset=UTF-8" -s "$mail_subject" "$recipients"
 }
+
