875638ff3649d75753df56692c9579714c3be1cd
Josué Tille <josue@tille.ch>
2020-02-22 00:48:55 +0100

    Use permission to protectect CAS server access

--- conf/add_sso_conf.py
+++ conf/add_sso_conf.py
@@ -2,10 +2,14 @@ import json
 
 with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
-    if "skipped_urls" in data:
+    if "skipped_urls" in data and "/_matrix" not in data:
         data["skipped_urls"].append("/_matrix")
     else:
         data["skipped_urls"] = ["/_matrix"]
+    if "protected_urls" in data and "/_matrix/cas_server.php/login" not in data:
+        data["protected_urls"].append("/_matrix/cas_server.php/login")
+    else:
+        data["protected_urls"] = ["/_matrix/cas_server.php/login"]
 
 with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
--- conf/remove_sso_conf.py
+++ conf/remove_sso_conf.py
@@ -3,6 +3,7 @@ import json
 with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
     data["skipped_urls"].remove("/_matrix")
+    data["protected_urls"].remove("/_matrix/cas_server.php/login")
 
 with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
