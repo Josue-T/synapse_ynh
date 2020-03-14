70b322f6e1b3db3c05ed9f63e91f303b7f625f70
Josué Tille <josue@tille.ch>
2020-03-14 14:35:20 +0100

    Add support for .well_known access

--- conf/add_sso_conf.py
+++ conf/add_sso_conf.py
@@ -1,15 +1,22 @@
 import json
 
+server_name = sys.argv[1]
+domain = sys.argv[2]
+
 with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
+
+    # Remove entry without the domain specified
+    data["skipped_urls"].remove("/_matrix")
+
     if "skipped_urls" in data and "/_matrix" not in data:
-        data["skipped_urls"].append("/_matrix")
+        data["skipped_urls"] += [domain + "/_matrix", server_name + "/.well-known/matrix/"]
     else:
-        data["skipped_urls"] = ["/_matrix"]
-    if "protected_urls" in data and "/_matrix/cas_server.php/login" not in data:
-        data["protected_urls"].append("/_matrix/cas_server.php/login")
+        data["skipped_urls"] = [domain + "/_matrix", server_name + "/.well-known/matrix/"]
+    if "protected_urls" in data and domain +  "/_matrix/cas_server.php/login" not in data:
+        data["protected_urls"].append(domain + "/_matrix/cas_server.php/login")
     else:
-        data["protected_urls"] = ["/_matrix/cas_server.php/login"]
+        data["protected_urls"] = [domain + "/_matrix/cas_server.php/login"]
 
 with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
--- conf/remove_sso_conf.py
+++ conf/remove_sso_conf.py
@@ -1,9 +1,16 @@
 import json
 
+server_name = sys.argv[1]
+domain = sys.argv[2]
+
 with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
-    data["skipped_urls"].remove("/_matrix")
-    data["protected_urls"].remove("/_matrix/cas_server.php/login")
+    for entry in data["skipped_urls"]:
+        if "/_matrix" in entry or "/.well-known/matrix/" in entry:
+            data["skipped_urls"].remove(entry)
+    for entry in data["protected_urls"]:
+        if "/_matrix" in entry:
+            data["protected_urls"].remove(entry)
 
 with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
