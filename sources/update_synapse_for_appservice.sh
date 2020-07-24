2d14c10c53869443776def0f53483381ef2285f8
Josué Tille <josue@tille.ch>
2020-07-24 23:33:53 +0200

    Add appservice support

---
+++ sources/update_synapse_for_appservice.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+app=__APP__
+service_config_file=/etc/matrix-$app/conf.d/app_service.yaml
+
+# Backup the previous config file
+cp $service_config_file /tmp/app_service_backup.yaml
+
+echo "app_service_config_files:" > $service_config_file
+for f in $(ls /etc/matrix-$app/app-service/); do
+    echo "  - /etc/matrix-synapse/$app" >> $service_config_file
+done
+
+# Set permissions
+chown matrix-$app $service_config_file
+chmod 600 $service_config_file
+
+systemctl restart matrix-$app
+
+if [ $? -eq 0 ]; then
+    rm /tmp/app_service_backup.yaml
+    exit 0
+else
+    echo "Failed to restart synapse with the new config file. Restore the old config file !!"
+    cp /tmp/app_service_backup.yaml $service_config_file
+fi
