385dd0c5dab47d5412e4461c89dc5089495d8034
Josué Tille <josue@tille.ch>
2020-11-03 22:20:47 +0100

    Revert "Merge pull request #218 from Gredin67/patch-1"
    This reverts commit 6773230733f3d4287dfaac5aeece7e24437fcc1e, reversing
    changes made to 0e43dd23f6785bc31015328f717bfc3dedc7ea6e.

--- sources/update_synapse_for_appservice.sh
+++ sources/update_synapse_for_appservice.sh
@@ -9,11 +9,10 @@ cp $service_config_file /tmp/app_service_backup.yaml
 echo "app_service_config_files:" > $service_config_file
 for f in $(ls /etc/matrix-$app/app-service/); do
     echo "  - /etc/matrix-$app/app-service/$f" >> $service_config_file
-    chmod 600 /etc/matrix-$app/app-service/$f
 done
 
 # Set permissions
-chown --reference=$service_config_file -R /etc/matrix-$app 
+chown matrix-$app $service_config_file
 chmod 600 $service_config_file
 
 systemctl restart matrix-$app
@@ -23,5 +22,5 @@ if [ $? -eq 0 ]; then
     exit 0
 else
     echo "Failed to restart synapse with the new config file. Restore the old config file !!"
-    mv /tmp/app_service_backup.yaml $service_config_file
+    cp /tmp/app_service_backup.yaml $service_config_file
 fi
