b3f27311a771957d7745d86c70151a181fa6aaea
Josué Tille <josue@tille.ch>
2021-02-14 15:20:07 +0100

    Update clean persistent scripts

--- scripts/remove_sso_conf_persistent.py
+++ scripts/remove_sso_conf_persistent.py
@@ -7,9 +7,15 @@ server_name = sys.argv[2]
 with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
 
-    data["skipped_urls"].remove("/_matrix")
-    data["skipped_urls"].remove("/.well-known/matrix/")
-    data["protected_urls"].remove("/_matrix/cas_server.php/login")
+    for domain in ("", sys.argv[1], sys.argv[2]):
+        for path in ("/_matrix", "/.well-known/matrix/", "/_matrix/cas_server.php/login"):
+            for l in (data["skipped_urls"],
+                      data["protected_urls"],
+                      data["permissions"]["custom_protected"]["uris"],
+                      data["permissions"]["custom_skipped"]["uris"]):
+            url = domain + path
+            while url in l:
+                l.remove(url)
 
 with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
