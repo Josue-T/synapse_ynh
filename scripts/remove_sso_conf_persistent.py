0f6c243f80d14c332fa5652a985972c6922c6dae
Josué Tille <josue@tille.ch>
2021-03-07 11:42:53 +0100

    Fix some small issues and remove hardcoded version of PHP

--- scripts/remove_sso_conf_persistent.py
+++ scripts/remove_sso_conf_persistent.py
@@ -1,21 +1,39 @@
 import json
 import sys
 
-domain = sys.argv[1]
-server_name = sys.argv[2]
-
 with open("/etc/ssowat/conf.json.persistent", "r", encoding='utf-8') as jsonFile:
     data = json.load(jsonFile)
 
     for domain in ("", sys.argv[1], sys.argv[2]):
         for path in ("/_matrix", "/.well-known/matrix/", "/_matrix/cas_server.php/login"):
-            for l in (data["skipped_urls"],
-                      data["protected_urls"],
-                      data["permissions"]["custom_protected"]["uris"],
-                      data["permissions"]["custom_skipped"]["uris"]):
             url = domain + path
-            while url in l:
-                l.remove(url)
+            try:
+                uri_list = data["skipped_urls"]
+                while url in uri_list:
+                    uri_list.remove(url)     
+            except:
+                pass
+
+            try:
+                uri_list = data["protected_urls"]
+                while url in uri_list:
+                    uri_list.remove(url)     
+            except:
+                pass
+
+            try:
+                uri_list = data["permissions"]["custom_protected"]["uris"]
+                while url in uri_list:
+                    uri_list.remove(url)     
+            except:
+                pass
+
+            try:
+                uri_list = data["permissions"]["custom_skipped"]["uris"]
+                while url in uri_list:
+                    uri_list.remove(url)     
+            except:
+                pass
 
 with open("/etc/ssowat/conf.json.persistent", "w", encoding='utf-8') as jsonFile:
     jsonFile.write(json.dumps(data, indent=4, sort_keys=True))
